5235a87c92041028c7b27adc40d2d4a8d1cd9a16
Yasmany Boffill <yboffill91@gmail.com>
2023-01-11 00:50:36 -0500

    Terminado maqueta

---
+++ script.js
@@ -0,0 +1,9 @@
+const abrir=document.getElementById("menHamb");
+const navMenu=document.getElementById("navMenu");
+const cerrar=document.getElementById("cerrar");
+abrir.addEventListener("click", ()=>{
+    navMenu.style.display="flex";
+})
+cerrar.addEventListener("click", ()=>{
+    navMenu.style.display="none";
+})
